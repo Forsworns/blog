f95208e8cae9b09fc342eb0f42c771b6eb5e8562
Forsworns <378974295@qq.com>
2019-07-20 17:46:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# blog
+My blog based on [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)+[Vuepress](https://vuepress.vuejs.org/guide/#how-it-works).
+
+Testing:
+
+```shell
+npm install
+npm run dev
+```
+
+Building:
+
+```shell
+npm build
+```
+
+Deploying:
+
+```shell
+./deploy.sh
+```
+
---
+++ blog/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // blog
+    title: `Kyrie's Blog`,
+    description: 'He is too lazy to write anything.',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'About Me', link: '/about-me/' },
+            { text: 'Blogs', link: '/blogs/' },
+        ]
+    },
+    // vuepress
+    serviceWorker: true,
+    evergreen: true,
+}
